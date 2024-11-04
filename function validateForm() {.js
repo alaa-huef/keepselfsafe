acb9a0181b326b172584d45518ac0b90979998c2
alaa-huef <dr.alaa532fgh@gmail.com>
2024-11-04 20:16:32 +0200

    Update function validateForm() {.js

--- function validateForm() {.js
+++ function validateForm() {.js
@@ -1,10 +1,14 @@
 function validateForm() {
-    var x = document.getElementById('mail').value;
-    if (x == "") {
+    var email = document.getElementById('mail').value;
+    
+    // Check if email is empty
+    if (email === "") {
         alert("Email must be filled out!");
         return false;
     } else {
-        document.getElementById("info").innerHTML = "Now we are in this together.<br> Thanks for subscribing and having us along on your health and wellness<br>journey.";
+        // Success message
+        document.getElementById("info").innerHTML = "Now we are in this together.<br> Thanks for subscribing and having us along on your health and wellness journey.";
         document.getElementById("info").style.color = "green";
+        return true;
     }
 }
